bc7ee2f701700819c4407ee2fcb02792309079c5
Bartek Kalinka <bartkali@hotmail.com>
2016-01-05 13:54:05 +0100

    current tree var in api

--- src/main/scala/pl/bka/Api.scala
+++ src/main/scala/pl/bka/Api.scala
@@ -1,15 +1,23 @@
 package pl.bka
 
 object Api {
-  def get(path: String): Node = Node(path)
+  var curtree: Node = null
 
-  def ser(tree: Node, jsonFilePath: String = "./tree.json"): Unit = {
+  def get(path: String): Node = {
+    curtree = Node(path)
+    curtree
+  }
+
+  def ser(tree: Node = curtree, jsonFilePath: String = "./tree.json"): Unit = {
     JsonOps.writeToJsonFile(tree, jsonFilePath)
     println("Done")
   }
 
-  def deser(jsonFilePath: String = "./tree.json"): Node = JsonOps.readFromJsonFile(jsonFilePath)
+  def deser(jsonFilePath: String = "./tree.json"): Node = {
+    curtree = JsonOps.readFromJsonFile(jsonFilePath)
+    curtree
+  }
 
-  def dup(tree:Node): Seq[Seq[Node]] = Duplicates.findDuplicates(tree)
+  def dup(tree:Node = curtree): Seq[Seq[Node]] = Duplicates.findDuplicates(tree)
 }
 
