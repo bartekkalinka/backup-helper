c11017729ccb01676f8b8d13c22512a1b1e3613c
Bartek Kalinka <bartkali@hotmail.com>
2016-01-07 15:26:55 +0100

    set tree in one place

--- src/main/scala/pl/bka/Api.scala
+++ src/main/scala/pl/bka/Api.scala
@@ -3,20 +3,20 @@ package pl.bka
 object Api {
   var tree: Node = null
 
-  def get(path: String): Node = {
-    tree = Node(path)
+  private def setTree(newTree: Node): Node = {
+    tree = newTree
     tree
   }
+
+  def get(path: String): Node = setTree(Node(path))
 
   def ser(tree: Node = tree, jsonFilePath: String = "./tree.json"): Unit = {
     JsonOps.writeToJsonFile(tree, jsonFilePath)
     println("Done")
   }
 
-  def deser(jsonFilePath: String = "./tree.json"): Node = {
-    tree = JsonOps.readFromJsonFile(jsonFilePath)
-    tree
-  }
+  def deser(jsonFilePath: String = "./tree.json"): Node =
+    setTree(JsonOps.readFromJsonFile(jsonFilePath))
 
   def dup(tree:Node = tree): Seq[Seq[Node]] = Duplicates.findDuplicates(tree)
 
